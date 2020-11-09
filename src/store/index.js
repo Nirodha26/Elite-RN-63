42357fce2e7eb1509dcb47c5623ab5c45a76228c
Nirodha Perera <NirodhaP@99x.lk>
2020-11-09 16:37:45 +0530

    Revert comments

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,21 @@ import { createStore, applyMiddleware } from 'redux'
 import moviesReducer from './reducer';
 
 // Custom Middleware
-// const asyncFunctionMiddleware = store => next => action => {
-//     // If the "action" is actually a function instead...
-//     if (typeof action === 'function') {
-//       // then call the function and pass `dispatch` and `getState` as arguments
-//       return action(store.dispatch, store.getState)
-//     }
+const asyncFunctionMiddleware = store => next => action => {
+    // If the "action" is actually a function instead...
+    if (typeof action === 'function') {
+      // then call the function and pass `dispatch` and `getState` as arguments
+      return action(store.dispatch, store.getState)
+    }
   
-//     // Otherwise, it's a normal action - send it onwards
-//     return next(action)
-//   }
+    // Otherwise, it's a normal action - send it onwards
+    return next(action)
+  }
 
 // Create a store enhanser to the dispatch method of the Redux store
-// const middlewareEnhancer = applyMiddleware(asyncFunctionMiddleware)
+const middlewareEnhancer = applyMiddleware(asyncFunctionMiddleware)
 
-export default store = createStore(moviesReducer)
+export default store = createStore(moviesReducer,middlewareEnhancer)
 
 
 
